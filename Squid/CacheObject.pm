175682f3b39a14f32e46ca4c200091438bafb709
dataking <dataking@gmail.com>
2015-07-30 14:39:50 -0700

    More additions.

--- Squid/CacheObject.pm
+++ Squid/CacheObject.pm
@@ -37,5 +37,81 @@ $VERSION	= 0.01;
 # This is a key for the URL
 my $KeyURL = '';
 
+# string
+#
+# This is a unique SHA hash for the cache object
+my $KeySHA = '';
 
+# string
+#
+# This is a unique MD5 hash for the cache object
+my $KeyMD5 = '';
+
+# string
+#
+# This is the original URL of the content that his cache object holds
+my $URL = '';
+
+# string
+#
+# This id the human readable file size of the cache object file
+my $Size = '';
+
+# string
+#
+# A human readable date representing the cache object's creation date
+my $Created = '';
+
+# string
+#
+# A human readable date representing the cache object's last modified date
+my $Modified = '';
+
+# string
+#
+# This is all the original HTTP headers as they appear in the cache object
+my $Headers = '';
+
+# string
+#
+# This is the cached content as it appears in the cache object
+my $Data = '';
+
+# Private Properties
+# FIX ME: Needs perl-ification
+
+# integer
+#
+# This is the length of the meta stat segment in the cache object file
+my $MetaDataLength = 0;
+
+# boolean
+#
+# This will determine if we parse all data or just the meta data headers in s Squid::CacheObject
+my $HeaderOnly = false;
+
+#####################################
+# 		F U N C T I O N S			#
+#####################################
+
+# constructor
+#
+# returns void
+sub SquidCacheObject($$) {
+	
+	$cacherObject = shift(@_);
+	$headerOnly = shift(@_) || false;
+
+	# If we have a cache object
+	if ($cacheObject) {
+		# no useless messages please
+		error_reporting('E_ERROR');
+
+		# set the header only flag
+		$self->HeaderOnly = $headerOnly;
+
+		# load it
+		$self->Load($cacheObject);
+	}
+}
 1;
